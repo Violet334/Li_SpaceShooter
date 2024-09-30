6b6874acbe24a80190062784ac42fd8d3ef59245
livio <livio@OA-S145-06>
2024-09-30 17:53:29 -0400

    began task3

--- SpaceShooter/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Moon.cs
@@ -16,10 +16,20 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        OrbitalMotion(rad, speed, planetTransform);
     }
-    void OrbitalMotion(float radius, float speed, Transform target)
+    void OrbitalMotion(float radius, float orbitalSpeed, Transform target)
     {
-
+        Vector3 velocity = Vector3.zero;
+        float i = 0;
+        i+=orbitalSpeed*Time.deltaTime;
+        Debug.Log(i);
+        if (i >= 360)
+        {
+            i = 0;
+        }
+        Vector3 angle = new Vector3(Mathf.Cos(i * Mathf.Deg2Rad), Mathf.Sin(i * Mathf.Deg2Rad)) * radius + target.position;
+        velocity += angle * orbitalSpeed * Time.deltaTime;
+        transform.position += velocity.normalized * orbitalSpeed * Time.deltaTime;
     }
 }
