c7ae9ffc0d39f6f68f50d637f74e13e71c1dfcf0
livio <livio@OA-S145-02>
2024-10-10 16:44:54 -0400

    look at target

---
+++ SpaceShooter/Assets/Scripts/Controllers/Missile.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Missile : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtTarget();
+        FollowPlayer();
+    }
+
+    void LookAtTarget()
+    {
+        Vector3 currentDir = transform.up;
+        float currentAngle = Mathf.Atan2(currentDir.y, currentDir.x) * Mathf.Rad2Deg;
+        Vector3 targetDir = target.position - transform.position;
+        float targetAngle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+
+        float deltaAngle = Mathf.DeltaAngle(currentAngle, targetAngle);
+        Debug.Log(deltaAngle);
+        if (deltaAngle > 0)
+        {
+            transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        }
+        else if (deltaAngle < 0)
+        {
+            transform.Rotate(0f, 0f, -speed * Time.deltaTime);
+        }
+    }
+
+    void FollowPlayer()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+        Vector3 targetDir = target.position - transform.position;
+        if (distance > 0.3)
+        {
+            transform.position += targetDir.normalized;
+        }
+        if(distance < 0.3)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
